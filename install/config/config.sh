ab149680b941ef868d89f561f7eaaa54662834d6
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-05-20 01:00:21 +0200

    Add default bash_profile

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,4 @@ cp -R ~/.local/share/omari/config/* ~/.config/
 
 # Configure the bash shell using Omari defaults
 cp ~/.local/share/omari/default/bashrc ~/.bashrc
+cp ~/.local/share/omari/default/bash_profile ~/.bash_profile
